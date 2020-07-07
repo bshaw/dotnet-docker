acf0ee47aad43348300e1474f5ba3cb8489c6b39
Brian Shaw <bshaw@hey.com>
2020-07-07 23:42:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotnet-docker
+
+A simple wrapper to run the .NET Core SDK and Runtime from a Docker container.
+
+Uses the official [Microsoft .NET Core SDK image](https://hub.docker.com/_/microsoft-dotnet-core-sdk/) using the `3.1` tag (Debian 10)
+
+Based on the Docker Compose [Run docker-compose in a container](https://github.com/docker/compose/blob/1.25.4/script/run/run.sh) script.
---
+++ run.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2020 Brian Shaw
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Run dotnet in a container
+#
+# This script will attempt to mirror the host paths by using volumes for the
+# following paths:
+#   * $(pwd)
+#   * $HOME if it's set
+#
+
+set -e
+
+VERSION="3.1"
+IMAGE="mcr.microsoft.com/dotnet/core/sdk:$VERSION"
+
+# Setup options for connecting to docker host
+if [ -z "$DOCKER_HOST" ]; then
+    DOCKER_HOST="/var/run/docker.sock"
+fi
+
+if [ -S "$DOCKER_HOST" ]; then
+    DOCKER_ADDR="-v $DOCKER_HOST:$DOCKER_HOST -e DOCKER_HOST"
+else
+    DOCKER_ADDR="-e DOCKER_HOST -e DOCKER_TLS_VERIFY -e DOCKER_CERT_PATH"
+fi
+
+# Setup volume mounts
+if [ "$(pwd)" != '/' ]; then
+    VOLUMES="-v $(pwd):$(pwd)"
+fi
+
+if [ -n "$HOME" ]; then
+    VOLUMES="$VOLUMES -v $HOME:$HOME -e HOME" # Pass in HOME to allow ~/-relative paths to work.
+fi
+
+# Only allocate tty if we detect one
+if [ -t 0 ] && [ -t 1 ]; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -t"
+fi
+
+# Always set -i to support piped and terminal input in run/exec
+DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -i"
+
+# Handle userns security
+if docker info --format '{{json .SecurityOptions}}' 2>/dev/null | grep -q 'name=userns'; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS --userns=host"
+fi
+
+# Run as the current user on Linux (the docker daemon runs as root)
+if [ "$(uname -s)" = 'Linux' ]; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -u $(id -u ${USER}):$(id -g ${USER})"
+fi
+
+# shellcheck disable=SC2086
+exec docker run --rm $DOCKER_RUN_OPTIONS $DOCKER_ADDR $VOLUMES -w "$(pwd)" $IMAGE dotnet "$@"
